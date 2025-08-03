44b5b6c018d25ca9ae392c369ea9a151bb78c13d
aprameyagoud <aprameyagundrathi@gmail.com>
2025-08-03 18:21:41 +0530

    Initial commit with Experiment 1

---
+++ Experiment 1/Exp_1.m
@@ -0,0 +1,69 @@
+clear all;
+close all;
+clc;
+
+I=imread('cat.jpg');
+[m,n,c]=size(I);
+Igr=zeros(m,n);
+for p=1:m
+    for q=1:n
+        rr=double(I(p,q,1));
+        gg=double(I(p,q,2));
+        bb=double(I(p,q,3));
+        Igr(p,q)=(rr+gg+bb)/3;
+    end
+end
+Igr = uint8(Igr);
+
+Ibw=zeros(m,n);
+mean_val = mean(Igr(:)); 
+
+for i=1:m
+    for j=1:n
+        if Igr(i,j) >= mean_val
+            Ibw(i,j) = 255;
+        else
+           Ibw(i,j) = 0;
+        end
+    end
+end
+Ibw = uint8(Ibw);
+
+Ir=I;
+Ir(:,:,2)=0;
+Ir(:,:,3)=0;
+
+Ig=I;
+Ig(:,:,1)=0;
+Ig(:,:,3)=0;
+
+Ib=I;
+Ib(:,:,2)=0;
+Ib(:,:,1)=0;
+
+
+
+subplot(2,3,1);
+imshow(I);
+title('Original RGB Image')
+
+subplot(2,3,2);
+imshow(Igr)
+title('Grayscale Image')
+
+subplot(2,3,3);
+imshow(Ibw)
+title('Black and White Image')
+
+subplot(2,3,4);
+imshow(Ir)
+title('Red Image')
+
+subplot(2,3,5);
+imshow(Ig)
+title('Green Image')
+
+subplot(2,3,6);
+imshow(Ib)
+title('Blue Image')
+
